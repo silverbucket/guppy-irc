38bc140e20de17fb724f076582ad145c069e857f
Nick Jennings <nick@silverbucket.net>
2013-12-05 22:39:17 +0100

    initial src file

---
+++ src/js/guppy-irc.js
@@ -0,0 +1,22 @@
+/**
+ * guppy-irc
+ *
+ * © 2013 Nick Jennings - nick@silverbucket.net
+ *
+ * guppy-irc is licensed under the AGPLv3.
+ * See the LICENSE file for details.
+ *
+ * The latest version of sockethub can be found here:
+ *   git://github.com/silverbucket/guppy-irc.git
+ *
+ * For more information about sockethub visit:
+ *   http://github.com/silverbucket/guppy-irc
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+(function (document, window, undefined) {
+
+
+})(document, window);

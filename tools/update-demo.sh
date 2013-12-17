77dd1c602d095c72839fd8473134da9ada7d245d
Nick Jennings <nick@silverbucket.net>
2013-12-17 14:58:57 +0100

    build fix

--- tools/update-demo.sh
+++ tools/update-demo.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir -p ../tmp/guppy-irc
 cp example/index.html ../tmp/guppy-irc/index.html &&
+cp -r logo/ ../tmp/guppy-irc/ &&
 cp src/js/guppy-irc.js ../tmp/guppy-irc/guppy-irc.js &&
 cp src/css/guppy-irc.css ../tmp/guppy-irc/guppy-irc.css &&
 git push &&
@@ -8,6 +9,8 @@ git checkout gh-pages &&
 cp ../tmp/guppy-irc/index.html example/ &&
 cp ../tmp/guppy-irc/guppy-irc.js src/js/ &&
 cp ../tmp/guppy-irc/guppy-irc.css src/css/ &&
+cp -r ../tmp/guppy-irc/logo . &&
+git add logo/ &&
 git commit -m "updating gh-pages demo" . &&
 git push origin gh-pages
 git checkout master
